700f97af20ea4f626ac4f857b7882b1b1b22f975
Yaskovich Dmitry <kadavr95@outlook.com>
2013-05-13 12:40:00 +0300

    -

--- LR16.c
+++ LR16.c
@@ -5,12 +5,14 @@
 
 int filling(int matrix[][m]);
 int output(int matrix[][m]);
+int rowsearch(int matrix[][m]);
 
 int main(void)
 {
 	int matrix[n][m];
 	filling(matrix);
 	output(matrix);
+	rowsearch(matrix);
 	fflush(stdin);//ожидание действий пользователя
 	getchar();
 	return 0;
@@ -25,16 +27,18 @@ int filling(int matrix[][m])
 	stime=(unsigned) ltime/2;
 	srand(stime);
 	for (i = 0; i < n; i++)//заполнение массива
-	for (j = 0; j < m; j++)
 	{
-	 if (i%2==0)
-	 {
-		matrix[i][j]=-rand()+2*rand();
-	 }
-	 else
-	 {
-		matrix[i][m-j-1]=-rand()+2*rand();
-     }
+		for (j = 0; j < m; j++)
+		{
+			if (i%2==0)
+			{
+				matrix[i][j]=-rand()+2*rand();
+			}
+			else
+			{
+				matrix[i][m-j-1]=-rand()+2*rand();
+			}
+		}
 	}
 }
 
@@ -45,10 +49,41 @@ int output(int matrix[][m])
 	for (i = 0; i < n; i++)
 	{
 		printf("|");
-	 for (j = 0; j < m; j++)
-	 {
-		printf("%6d|",matrix[i][j]);
-	 }
-	 printf("\n");
+		for (j = 0; j < m; j++)
+		{
+			printf("%6d|",matrix[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+int rowsearch(int matrix[][m])
+{
+	int sum,maxsum,i,j;
+	maxsum=-m*100000;
+	for (i = 0; i < n; i++)
+	{
+		sum=0;
+		for (j = 0; j < m; j++)
+		{
+			sum=sum+matrix[i][j];
+		}
+		if (sum>maxsum)
+		{
+			maxsum=sum;
+		}
+	}
+	printf("Rows with maximum sum of elements: ");
+	for (i = 0; i < n; i++)
+	{
+		sum=0;
+		for (j = 0; j < m; j++)
+		{
+			sum=sum+matrix[i][j];
+		}
+		if (sum==maxsum)
+		{
+			printf("%d ",i+1);
+		}
 	}
 }
